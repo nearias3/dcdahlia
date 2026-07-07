542f59211891da44cadd6471bbb6ebdfe2cd67e2
nearias3 <127260408+nearias3@users.noreply.github.com>
2026-07-07 16:47:26 -0400

    Customized the 404 not found error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+import Container from "../components/ui/Container";
+
 export default function NotFound() {
-  return <h1>Not Found</h1>;
+  return (
+    <Container>
+      <section className="not-found">
+        <p className="eyebrow">Case File Missing</p>
+        <h1>404</h1>
+        <p>This file seems to have disappeared from the evidence locker.</p>
+
+        <Link className="button-link" to="/">
+          Return Home
+        </Link>
+      </section>
+    </Container>
+  );
 }
