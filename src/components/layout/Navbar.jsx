77281577eef5db01c24c04ebe9af5356d1972fbf
nearias3 <127260408+nearias3@users.noreply.github.com>
2026-07-07 16:36:54 -0400

    added hamburger style navbar toggle

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,22 +1,50 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import site from "../../data/site";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
   return (
     <header className="navbar">
       <div className="container navbar__inner">
-        <NavLink to="/" className="navbar__brand">
+        <NavLink to="/" className="navbar__brand" onClick={closeMenu}>
           {site.author.name}
         </NavLink>
 
-        <nav className="navbar__links" aria-label="Main navigation">
-          <NavLink to="/" end>
+        <button
+          className="navbar__toggle"
+          type="button"
+          aria-label="Toggle navigation menu"
+          aria-expanded={isMenuOpen}
+          onClick={() => setIsMenuOpen((current) => !current)}
+        >
+          ☰
+        </button>
+
+        <nav
+          className={`navbar__links ${isMenuOpen ? "is-open" : ""}`}
+          aria-label="Main navigation"
+        >
+          <NavLink to="/" end onClick={closeMenu}>
             Home
           </NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/books">Books</NavLink>
-          <NavLink to="/events">Events</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/about" onClick={closeMenu}>
+            About
+          </NavLink>
+          <NavLink to="/books" onClick={closeMenu}>
+            Books
+          </NavLink>
+          <NavLink to="/events" onClick={closeMenu}>
+            Events
+          </NavLink>
+          <NavLink to="/contact" onClick={closeMenu}>
+            Contact
+          </NavLink>
         </nav>
       </div>
     </header>
