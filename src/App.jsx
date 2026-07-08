a48cda3bca712485c887bccab23975f0d7cd9b3a
nearias3 <127260408+nearias3@users.noreply.github.com>
2026-07-08 16:10:11 -0400

    polished featured book section and fixed autoscroll from homepage link to book

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// src/App.jsx
-
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
@@ -9,19 +7,24 @@ import BookDetail from "./pages/BookDetail";
 import Events from "./pages/Events";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ui/ScrollToTop";
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/books" element={<Books />} />
-        <Route path="/books/:slug" element={<BookDetail />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/books/:slug" element={<BookDetail />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
